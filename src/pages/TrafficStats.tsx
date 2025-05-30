afc78f22b6a2492f974ae0dd1f9364a3465467de
lven949 <lven52651@gmail.com>
2025-05-30 12:15:01 +0800

    Update TrafficStats.tsx

--- src/pages/TrafficStats.tsx
+++ src/pages/TrafficStats.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
