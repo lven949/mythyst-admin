09282a75db85bac73151c26e821b6549c1d5d951
lven949 <lven52651@gmail.com>
2025-05-30 12:15:14 +0800

    Update Users.tsx

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 import { Eye, ArrowUpDown, Loader2, AlertCircle, Pencil, X, Save } from 'lucide-react';
 
 interface User {
