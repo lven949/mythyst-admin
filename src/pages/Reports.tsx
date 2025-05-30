55151ae7b713a9a7d967ef63502260b15c788919
lven949 <lven52651@gmail.com>
2025-05-30 12:09:10 +0800

    Update Reports.tsx

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 import { Edit2, Trash2, CheckCircle, AlertCircle, WifiOff, RefreshCw, ArrowUpDown } from 'lucide-react';
 
 interface BookReport {
