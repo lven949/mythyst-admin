ca6507a226fa8b3b645253e9266b0d82875cd524
lven949 <lven52651@gmail.com>
2025-05-30 12:10:06 +0800

    Update Coins.tsx

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 import { ArrowUpDown, Calendar, Search, Download, Filter, Loader2 } from 'lucide-react';
 
 interface CoinTransaction {
